e08a0f92b66dfd48ab0cc5c77e2b50a1207c87e9
Bohdan Lyzanets <bogdan.uz@gmail.com>
2017-12-21 18:50:50 +0200

    update `interpolate` pattern and `varname`

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const dot = require('dot');
 
 exports.default = function (source) {
+  dot.templateSettings.varname = 'props';
+  dot.templateSettings.interpolate = /\$\{([\s\S]+?)\}/g;
   dot.templateSettings.selfcontained = true;
 
   return 'export default ' + dot.template(source);
